19a7bbf0626c8c1dd2d3ec8328db43b6b3e043f0
Moses Mwangi <moses.soft.eng@gmail.com>
2023-05-30 21:43:35 +0300

    Add show SHOW_LOGS config

--- app/src/main/java/status/four/saves/boost/guaranteed/shared/Config.java
+++ app/src/main/java/status/four/saves/boost/guaranteed/shared/Config.java
@@ -2,5 +2,6 @@ package status.four.saves.boost.guaranteed.shared;
 
 public class Config {
     public static final String APP_NAME_ID = "status.four.saves.boost.guaranteed";
+    public static boolean SHOW_LOGS = true;
     public static final String SHARED_PREFS_NAME = APP_NAME_ID;
 }
