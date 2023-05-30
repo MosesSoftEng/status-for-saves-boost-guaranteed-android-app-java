6b8e450e8e616241e55ad985d0015e85210ea94e
Moses Mwangi <moses.soft.eng@gmail.com>
2023-05-30 14:23:14 +0300

    Create LoginActivity

---
+++ app/src/main/java/status/four/saves/boost/guaranteed/presentation/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package status.four.saves.boost.guaranteed.presentation.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import status.four.saves.boost.guaranteed.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
