abc6c96e032360217d4d9edc8de47fe84808e882
Moses Mwangi <moses.soft.eng@gmail.com>
2023-05-30 14:16:10 +0300

    Create StartActivity as launcher activity

---
+++ app/src/main/java/status/four/saves/boost/guaranteed/presentation/start/StartActivity.java
@@ -0,0 +1,16 @@
+package status.four.saves.boost.guaranteed.presentation.start;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import status.four.saves.boost.guaranteed.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
