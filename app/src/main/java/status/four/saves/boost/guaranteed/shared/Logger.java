aee3869de6e9850401f2c74c493b59576c8f6bf9
Moses Mwangi <moses.soft.eng@gmail.com>
2023-05-30 21:43:08 +0300

    Add different logs types methods

---
+++ app/src/main/java/status/four/saves/boost/guaranteed/shared/Logger.java
@@ -0,0 +1,31 @@
+package status.four.saves.boost.guaranteed.shared;
+
+import static status.four.saves.boost.guaranteed.shared.Config.APP_NAME_ID;
+import static status.four.saves.boost.guaranteed.shared.Config.SHOW_LOGS;
+
+import android.util.Log;
+
+
+public class Logger {
+    private static final String TAG = APP_NAME_ID + ".logs";
+
+    public static void d(String message) {
+        if(SHOW_LOGS) Log.d(TAG, message);
+    }
+
+    public static void e(String message) {
+        if(SHOW_LOGS) Log.e(TAG, message);
+    }
+
+    public static void i(String message) {
+        if(SHOW_LOGS) Log.i(TAG, message);
+    }
+
+    public static void w(String message) {
+        if(SHOW_LOGS) Log.w(TAG, message);
+    }
+
+    public static void i(String location, String message) {
+        if(SHOW_LOGS) Log.i(TAG, location + ": " +message);
+    }
+}
