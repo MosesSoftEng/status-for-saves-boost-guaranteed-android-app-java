3ae59a2e1278db06ec3cf43f73e729412d4ef1df
Moses Mwangi <moses.soft.eng@gmail.com>
2023-05-31 21:12:27 +0300

    Add dash activity

---
+++ app/src/main/java/status/four/saves/boost/guaranteed/presentation/dash/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package status.four.saves.boost.guaranteed.presentation.dash.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
