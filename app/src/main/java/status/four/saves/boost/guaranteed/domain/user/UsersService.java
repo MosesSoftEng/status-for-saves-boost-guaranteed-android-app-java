3c613ac4b3547fba2c2af952fff1bca8405dfc7d
Moses Mwangi <moses.soft.eng@gmail.com>
2023-05-31 20:56:23 +0300

    Create UsersService.java

---
+++ app/src/main/java/status/four/saves/boost/guaranteed/domain/user/UsersService.java
@@ -0,0 +1,101 @@
+package status.four.saves.boost.guaranteed.domain.user;
+
+import static status.four.saves.boost.guaranteed.shared.Config.API_URL;
+import static status.four.saves.boost.guaranteed.shared.Config.SHARED_PREFS_KEY_USER_WHATSAPP_MOBILE_NUMBER;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import status.four.saves.boost.guaranteed.data.storage.SharedPreferencesHelper;
+import status.four.saves.boost.guaranteed.shared.VolleyHelper;
+
+public class UsersService {
+    private static UsersService instance;
+    private static SharedPreferencesHelper sharedPreferencesHelper;
+    private static VolleyHelper volleyHelper;
+
+    /**
+     * Constructs a new instance of UsersService.
+     *
+     * @param context The application context.
+     */
+    public UsersService(Context context) {
+        sharedPreferencesHelper = new SharedPreferencesHelper(context);
+        volleyHelper = VolleyHelper.getInstance(context);
+    }
+
+    /**
+     * Gets the singleton instance of UsersService.
+     *
+     * @param context The application context.
+     * @return The instance of UsersService.
+     */
+    public static synchronized UsersService getInstance(Context context) {
+        if (instance == null) {
+            instance = new UsersService(context);
+        }
+        return instance;
+    }
+
+    /**
+     * Performs user login with the provided WhatsApp phone number.
+     *
+     * @param whatsAppPhoneNumber The WhatsApp phone number to log in with.
+     * @param callback            The callback to handle the login result.
+     */
+    public static void loginUser(String whatsAppPhoneNumber, Callback callback) {
+        callback.onSuccess("Number saved");
+
+        try {
+            JSONObject requestBody = new JSONObject();
+            requestBody.put("whatsAppPhoneNumber", whatsAppPhoneNumber);
+
+            // TODO: Create UserAPI class
+            volleyHelper.makeRequest(
+                    API_URL + "/auth",
+                    Request.Method.POST,
+                    null,
+                    requestBody,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            sharedPreferencesHelper.saveString(SHARED_PREFS_KEY_USER_WHATSAPP_MOBILE_NUMBER, whatsAppPhoneNumber);
+
+                            callback.onSuccess(response.toString());
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            callback.onError(error);
+                        }
+                    });
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * The callback interface for handling login results.
+     */
+    public interface Callback {
+        /**
+         * Called when the login operation is successful.
+         *
+         * @param message The success message.
+         */
+        void onSuccess(String message);
+
+        /**
+         * Called when an error occurs during the login operation.
+         *
+         * @param throwable The error that occurred.
+         */
+        void onError(Throwable throwable);
+    }
+}
