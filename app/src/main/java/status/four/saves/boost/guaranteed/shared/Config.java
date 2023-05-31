1d59aef9a051052ceba7fa8aadee8505722b895f
Moses Mwangi <moses.soft.eng@gmail.com>
2023-05-31 20:49:42 +0300

    Add api url config

--- app/src/main/java/status/four/saves/boost/guaranteed/shared/Config.java
+++ app/src/main/java/status/four/saves/boost/guaranteed/shared/Config.java
@@ -3,5 +3,11 @@ package status.four.saves.boost.guaranteed.shared;
 public class Config {
     public static final String APP_NAME_ID = "status.four.saves.boost.guaranteed";
     public static boolean SHOW_LOGS = true;
+
+    // Network
+    public static final String API_URL = "https://kt99gjkxv5.execute-api.us-east-1.amazonaws.com/dev/v1";
+
+    // Storage
     public static final String SHARED_PREFS_NAME = APP_NAME_ID;
+    public static final String SHARED_PREFS_KEY_USER_WHATSAPP_MOBILE_NUMBER = "SHARED_PREFS_kEY_USER_WHATSAPP_MOBILE_NUMBER";
 }
