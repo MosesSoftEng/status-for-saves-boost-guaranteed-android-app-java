b99f8843391d389925e8233507ea63550676d84d
Moses Mwangi <moses.soft.eng@gmail.com>
2023-05-31 21:06:12 +0300

    Add check for login

--- app/src/main/java/status/four/saves/boost/guaranteed/presentation/start/StartActivity.java
+++ app/src/main/java/status/four/saves/boost/guaranteed/presentation/start/StartActivity.java
@@ -2,15 +2,28 @@ package status.four.saves.boost.guaranteed.presentation.start;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import status.four.saves.boost.guaranteed.R;
+import status.four.saves.boost.guaranteed.domain.user.UsersService;
+import status.four.saves.boost.guaranteed.presentation.login.LoginActivity;
+import status.four.saves.boost.guaranteed.shared.Logger;
 
 public class StartActivity extends AppCompatActivity {
+    UsersService usersService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        // TODO: Check if terms and private policy are accepted.
+        usersService = new UsersService(getApplicationContext());
+
+        if(usersService.isUserLoggedIn())
+            Logger.i("user logged in");
+        else
+            startActivity(new Intent(StartActivity.this, LoginActivity.class));
     }
 }
