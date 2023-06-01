09f65a287d23ddd3183dfdaadbdd1799d657541d
Moses Mwangi <moses.soft.eng@gmail.com>
2023-06-01 22:14:37 +0300

    Create NewUsersRecyclerViewAdapter.java

---
+++ app/src/main/java/status/four/saves/boost/guaranteed/presentation/dash/ui/users/NewUsersRecyclerViewAdapter.java
@@ -0,0 +1,6 @@
+package status.four.saves.boost.guaranteed.presentation.dash.ui.users;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NewUsersRecyclerViewAdapter extends RecyclerView {
+}
