17115846075add68fbfc44df245f8a0f4f5d147a
Moses Mwangi <moses.soft.eng@gmail.com>
2023-06-01 23:55:00 +0300

    Create SaveBackViewModel.java

---
+++ app/src/main/java/status/four/saves/boost/guaranteed/presentation/dash/ui/users/SaveBackViewModel.java
@@ -0,0 +1,10 @@
+package status.four.saves.boost.guaranteed.presentation.dash.ui.users;
+
+import androidx.lifecycle.ViewModel;
+
+
+public class SaveBackViewModel extends ViewModel {
+    public SaveBackViewModel() {
+
+    }
+}
