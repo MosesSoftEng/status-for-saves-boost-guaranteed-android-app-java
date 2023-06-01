c7a45067516fa200c803b27036495e9ee4024c3b
Moses Mwangi <moses.soft.eng@gmail.com>
2023-06-01 23:55:18 +0300

    Delete NewUsersRecyclerViewAdapter.java

--- app/src/main/java/status/four/saves/boost/guaranteed/presentation/dash/ui/users/NewUsersRecyclerViewAdapter.java
+++
@@ -1,6 +0,0 @@
-package status.four.saves.boost.guaranteed.presentation.dash.ui.users;
-
-import androidx.recyclerview.widget.RecyclerView;
-
-public class NewUsersRecyclerViewAdapter extends RecyclerView {
-}
