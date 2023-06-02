df5ff8f9f1c9dbff4783d0ee6532807efd509264
Moses Mwangi <moses.soft.eng@gmail.com>
2023-06-02 15:03:26 +0300

    Add swipe to refresh functionality
    Update the DashboardFragment class to add swipe to refresh functionality using SwipeRefreshLayout. The user list is cleared and new users are fetched when the swipe to refresh gesture is performed. The updateUserList method is also modified to stop the refreshing animation.

--- app/src/main/java/status/four/saves/boost/guaranteed/presentation/dash/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/status/four/saves/boost/guaranteed/presentation/dash/ui/dashboard/DashboardFragment.java
@@ -11,6 +11,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
 
@@ -19,18 +20,33 @@ import status.four.saves.boost.guaranteed.domain.user.User;
 import status.four.saves.boost.guaranteed.shared.Logger;
 
 
+/**
+ * Fragment class for displaying the dashboard screen.
+ */
 public class DashboardFragment extends Fragment {
-    DashboardViewModel dashboardViewModel;
+    private DashboardViewModel dashboardViewModel;
     private FragmentDashboardBinding binding;
+    private SwipeRefreshLayout newUsersSwipeRefreshLayout;
     private RecyclerView newUserRecyclerView;
     private NewUsersRecyclerViewAdapter newUsersRecyclerViewAdapter;
 
+    /**
+     * Creates the view for the dashboard fragment.
+     *
+     * @param inflater           The layout inflater object that can be used to inflate any views in the fragment.
+     * @param container          The parent view that the fragment's UI should be attached to.
+     * @param savedInstanceState The saved instance state of the fragment, or null if this is a new instance.
+     * @return The root view of the fragment's layout.
+     */
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        newUsersSwipeRefreshLayout = binding.newUsersSwipeRefreshLayout;
+        newUsersSwipeRefreshLayout.setOnRefreshListener(this::refreshData);
 
         newUsersRecyclerViewAdapter = new NewUsersRecyclerViewAdapter();
         newUserRecyclerView = binding.newUsersRecyclerView;
@@ -40,16 +56,45 @@ public class DashboardFragment extends Fragment {
 
         dashboardViewModel.getUsers().observe(getViewLifecycleOwner(), this::updateUserList);
 
-        dashboardViewModel.fetchUsers();
+        fetchUsers();
 
         return root;
     }
 
-    private void updateUserList(ArrayList<User> users) {
-        newUsersRecyclerViewAdapter.setData(users);
-        newUsersRecyclerViewAdapter.notifyDataSetChanged();
+    /**
+     * Fetches the list of users from the ViewModel.
+     */
+    private void fetchUsers() {
+        newUsersSwipeRefreshLayout.setRefreshing(true);
+        dashboardViewModel.fetchUsers();
     }
 
+    /**
+     * Updates the user list in the RecyclerView adapter.
+     *
+     * @param users The list of users to be displayed.
+     */
+    private void updateUserList(ArrayList<User> users) {
+        Logger.d("DashboardFragment updateUserList, users: " + users.toString());
+
+        newUsersRecyclerViewAdapter.setData(users);
+        newUsersRecyclerViewAdapter.notifyDataSetChanged();
+        newUsersSwipeRefreshLayout.setRefreshing(false);
+    }
+
+    /**
+     * Refreshes the data by clearing the user list and fetching new users.
+     */
+    private void refreshData() {
+        dashboardViewModel.clearUsers();
+        fetchUsers();
+    }
+
+    /**
+     * Returns an instance of RecyclerView.OnScrollListener for handling scroll events in the RecyclerView.
+     *
+     * @return The RecyclerView.OnScrollListener instance.
+     */
     private RecyclerView.OnScrollListener recyclerViewOnScrollListener() {
         return new RecyclerView.OnScrollListener() {
             @Override
@@ -59,7 +104,7 @@ public class DashboardFragment extends Fragment {
                 if (!recyclerView.canScrollVertically(1)) {
                     Logger.d("DashboardFragment recyclerViewOnScrollListener onScrolled, !recyclerView.canScrollVertically");
 
-                    dashboardViewModel.fetchUsers();
+                    fetchUsers();
                 }
             }
         };
