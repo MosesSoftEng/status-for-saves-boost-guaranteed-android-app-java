6a102c87436c476f424d0f697dff6c595d13aa3f
Moses Mwangi <moses.soft.eng@gmail.com>
2023-06-02 16:45:52 +0300

    Create NewUsersViewModelFactory.java

---
+++ app/src/main/java/status/four/saves/boost/guaranteed/presentation/dash/ui/dashboard/NewUsersViewModelFactory.java
@@ -0,0 +1,25 @@
+package status.four.saves.boost.guaranteed.presentation.dash.ui.dashboard;
+
+import android.app.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+public class NewUsersViewModelFactory implements ViewModelProvider.Factory {
+    private final Activity activity;
+
+    public NewUsersViewModelFactory(Activity activity) {
+        this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(DashboardViewModel.class)) {
+            return (T) new DashboardViewModel(activity.getApplication(), activity);
+        }
+        throw new IllegalArgumentException("Unknown ViewModel class: " + modelClass.getName());
+    }
+}
+
