6956c229b495ab5e183301aba3097b1c37656eac
Moses Mwangi <moses.soft.eng@gmail.com>
2023-06-03 15:01:56 +0300

    Create SavedViewModelFactory.java

---
+++ app/src/main/java/status/four/saves/boost/guaranteed/presentation/dash/ui/Saved/SavedViewModelFactory.java
@@ -0,0 +1,26 @@
+package status.four.saves.boost.guaranteed.presentation.dash.ui.Saved;
+
+import android.app.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import status.four.saves.boost.guaranteed.presentation.dash.ui.newUsers.NewUserViewModel;
+
+public class SavedViewModelFactory implements ViewModelProvider.Factory {
+    private final Activity activity;
+
+    public SavedViewModelFactory(Activity activity) {
+        this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(NewUserViewModel.class)) {
+            return (T) new NewUserViewModel(activity.getApplication(), activity);
+        }
+        throw new IllegalArgumentException("Unknown ViewModel class: " + modelClass.getName());
+    }
+}
