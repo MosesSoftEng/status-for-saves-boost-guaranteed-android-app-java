80a11b4e1b319c8cb8a92b9b5b40d0c17f41c954
Moses Mwangi <moses.soft.eng@gmail.com>
2023-06-03 16:11:42 +0300

    Add RecyclerView and SwipeRefreshLayout to SavedFragment
    Added RecyclerView and SwipeRefreshLayout to the SavedFragment to display the list of saved contacts. The SavedRecyclerViewAdapter is set as the adapter for the RecyclerView, and the updateContactsList() method is implemented to update the contact list when data changes.

--- app/src/main/java/status/four/saves/boost/guaranteed/presentation/dash/ui/Saved/SavedFragment.java
+++ app/src/main/java/status/four/saves/boost/guaranteed/presentation/dash/ui/Saved/SavedFragment.java
@@ -8,12 +8,24 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import java.util.ArrayList;
+
 import status.four.saves.boost.guaranteed.databinding.FragmentSavedBinding;
+import status.four.saves.boost.guaranteed.domain.contact.Contact;
+import status.four.saves.boost.guaranteed.presentation.dash.ui.newUsers.NewUsersRecyclerViewAdapter;
 
 public class SavedFragment extends Fragment {
     SavedViewModel savedViewModel;
 
     private FragmentSavedBinding binding;
+
+    private SwipeRefreshLayout savedSwipeRefreshLayout;
+    private RecyclerView savedRecyclerView;
+    private SavedRecyclerViewAdapter savedRecyclerViewAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -22,9 +34,22 @@ public class SavedFragment extends Fragment {
         binding = FragmentSavedBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        savedRecyclerViewAdapter = new SavedRecyclerViewAdapter(savedViewModel);
+
+        savedRecyclerView = binding.savedRecyclerView;
+        savedRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        savedRecyclerView.setAdapter(savedRecyclerViewAdapter);
+
+        savedViewModel.getContacts().observe(getViewLifecycleOwner(), this::updateContactsList);
+
         savedViewModel.fetchContacts();
 
         return root;
+    }
+
+    private void updateContactsList(ArrayList<Contact> contacts) {
+        savedRecyclerViewAdapter.setData(contacts);
+        savedRecyclerViewAdapter.notifyDataSetChanged();
     }
 
     @Override
