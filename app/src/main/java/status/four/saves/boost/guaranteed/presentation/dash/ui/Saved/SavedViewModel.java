cbfdbe23114fd24330d3d84ef8e1c0f7a0d574bc
Moses Mwangi <moses.soft.eng@gmail.com>
2023-06-03 14:57:23 +0300

    Add AndroidViewModel to SavedViewModel
    Update the SavedViewModel class to extend AndroidViewModel and pass the Application context to the superclass constructor.

--- app/src/main/java/status/four/saves/boost/guaranteed/presentation/dash/ui/Saved/SavedViewModel.java
+++ app/src/main/java/status/four/saves/boost/guaranteed/presentation/dash/ui/Saved/SavedViewModel.java
@@ -1,17 +1,22 @@
 package status.four.saves.boost.guaranteed.presentation.dash.ui.Saved;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
 import java.util.ArrayList;
-
 import status.four.saves.boost.guaranteed.domain.contact.Contact;
 
-public class SavedViewModel extends ViewModel {
+public class SavedViewModel extends AndroidViewModel {
+
     private final MutableLiveData<ArrayList<Contact>> contacts;
 
-    public SavedViewModel() {
+    public SavedViewModel(@NonNull Application application) {
+        super(application);
+
         contacts = new MutableLiveData<>();
     }
 
