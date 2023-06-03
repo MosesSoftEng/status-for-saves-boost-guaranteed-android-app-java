658e04c1e27a3eea89bb36a171ce3b825f01abe7
Moses Mwangi <moses.soft.eng@gmail.com>
2023-06-03 15:41:45 +0300

    Add ContactsRepo dependency to SavedViewModel
    Added the ContactsRepo dependency to the SavedViewModel class to fetch and provide contact data. The ContactsRepo instance is initialized in the constructor, and the fetchContacts() method is implemented to update the LiveData with the fetched contacts.

--- app/src/main/java/status/four/saves/boost/guaranteed/presentation/dash/ui/Saved/SavedViewModel.java
+++ app/src/main/java/status/four/saves/boost/guaranteed/presentation/dash/ui/Saved/SavedViewModel.java
@@ -1,5 +1,6 @@
 package status.four.saves.boost.guaranteed.presentation.dash.ui.Saved;
 
+import android.app.Activity;
 import android.app.Application;
 
 import androidx.annotation.NonNull;
@@ -8,19 +9,28 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import java.util.ArrayList;
+
+import status.four.saves.boost.guaranteed.data.storage.ContactsRepo;
 import status.four.saves.boost.guaranteed.domain.contact.Contact;
 
 public class SavedViewModel extends AndroidViewModel {
+    ContactsRepo contactsRepo;
 
     private final MutableLiveData<ArrayList<Contact>> contacts;
 
-    public SavedViewModel(@NonNull Application application) {
+    public SavedViewModel(@NonNull Application application, Activity activity) {
         super(application);
+
+        contactsRepo = ContactsRepo.getInstance(activity);
 
         contacts = new MutableLiveData<>();
     }
 
     public LiveData<ArrayList<Contact>> getContacts() {
         return contacts;
+    }
+
+    public void fetchContacts() {
+        contacts.setValue(contactsRepo.getContacts());
     }
 }
