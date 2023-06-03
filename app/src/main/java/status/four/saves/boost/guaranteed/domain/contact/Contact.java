3232d90a203710422d73ffdb77916427ab2ed2b2
Moses Mwangi <moses.soft.eng@gmail.com>
2023-06-03 15:42:45 +0300

    Implement class members

--- app/src/main/java/status/four/saves/boost/guaranteed/domain/contact/Contact.java
+++ app/src/main/java/status/four/saves/boost/guaranteed/domain/contact/Contact.java
@@ -1,4 +1,27 @@
 package status.four.saves.boost.guaranteed.domain.contact;
 
 public class Contact {
+    private String name;
+    private String phone;
+
+    public Contact(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "name=" + name +
+                ", phone=" + phone +
+                '}';
+    }
 }
