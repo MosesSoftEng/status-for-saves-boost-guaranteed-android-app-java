3444a69335e600c8338404fb2827b2dee0436e56
Moses Mwangi <moses.soft.eng@gmail.com>
2023-06-19 22:24:14 +0300

    Rename URLs configs

--- app/src/main/java/status/four/saves/boost/guaranteed/shared/Config.java
+++ app/src/main/java/status/four/saves/boost/guaranteed/shared/Config.java
@@ -6,8 +6,8 @@ public class Config {
 
     // Links & Network
     public static final String API_URL = "https://ywu7goj8i4.execute-api.us-east-1.amazonaws.com/dev/v1";
-    public static final String PRIVATE_POLICY_LINK = "https://zawadizggo.blogspot.com/2023/06/status-for-saves-boost-guaranteed-private-policy.html";
-    public static final String TERMS_OF_USE_LINK = "https://zawadizggo.blogspot.com/2023/06/status-for-saves-boost-guaranteed-terms-of-use.html";
+    public static final String URL_PRIVATE_POLICY = "https://zawadizggo.blogspot.com/2023/06/status-for-saves-boost-guaranteed-private-policy.html";
+    public static final String URL_TERMS_OF_USE = "https://zawadizggo.blogspot.com/2023/06/status-for-saves-boost-guaranteed-terms-of-use.html";
 
     // Storage
     public static final String SHARED_PREFS_NAME = APP_NAME_ID;
